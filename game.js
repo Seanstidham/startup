4ba052d272ae954724c244c02db7853164b12eac
Sean Stidham <seanstidham14@gmail.com>
2023-03-10 18:54:47 -0700

    game css and js edit

--- game.js
+++ game.js
@@ -6,20 +6,23 @@ let spinCount = 0;
 function rotateImage() {
   spinCount++;
   counterInput.value = spinCount;
-  
+
   const isLoser = Math.floor(Math.random() * 6) === 0;
-  
+
   if (isLoser) {
     document.body.appendChild(document.createTextNode('Fail'));
     spinButton.disabled = true;
     counterInput.disabled = true;
   } else {
     image.classList.add('spin');
-    
-    const currentRotation = parseInt(image.style.transform.replace(/[^0-9\-]/g,''));
-    const newRotation = (currentRotation + 30) % 360;
-    
-    image.style.transform = `rotate(${newRotation}deg)`;
+    setTimeout(() => {
+      const currentRotation = parseInt(image.style.transform.replace(/[^0-9\-]/g,''));
+      const newRotation = (currentRotation + 30) % 360;
+      image.style.transform = `rotate(${newRotation}deg)`;
+      image.classList.remove('spin');
+
+      document.body.appendChild(document.createTextNode('Winner'));
+    }, 1000);
   }
 }
 
