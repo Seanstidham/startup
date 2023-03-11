cc9c8133494d32e0d2e5ae42e5d2947059bbe595
Sean Stidham <seanstidham14@gmail.com>
2023-03-10 19:29:24 -0700

    js edit

--- scores.js
+++ scores.js
@@ -1,45 +1,73 @@
-const image = document.querySelector('img');
-const spinButton = document.querySelector('#spin-button');
-const counterInput = document.querySelector('#num-spins');
-const message = document.querySelector('#message');
-let spinCount = 0;
-
-function rotateImage() {
-  spinCount++;
-  counterInput.value = spinCount;
-
-  const isLoser = Math.floor(Math.random() * 6) === 0;
-
-  if (isLoser) {
-    const failMessage = 'Fail';
-    message.appendChild(document.createTextNode(failMessage));
-
-    // Get player name
-    const playerName = prompt('Enter your name:');
-    if (playerName) {
-      // Get current date
-      const currentDate = new Date();
-      const formattedDate = currentDate.toLocaleDateString();
-      
-      // Print player score
-      const scoreMessage = `${playerName} scored ${spinCount} spins before receiving a ${failMessage} on ${formattedDate}.`;
-      message.appendChild(document.createElement('br'));
-      message.appendChild(document.createTextNode(scoreMessage));
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
     }
-
-    spinButton.disabled = true;
-    counterInput.disabled = true;
-  } else {
-    image.classList.add('spin');
-    setTimeout(() => {
-      const currentRotation = parseInt(image.style.transform.replace(/[^0-9\-]/g,''));
-      const newRotation = (currentRotation + 30) % 360;
-      image.style.transform = `rotate(${newRotation}deg)`;
-      image.classList.remove('spin');
-
-      document.body.appendChild(document.createTextNode('Winner '));
-    }, 1000);
+  //first checks the storage to see if theres data
+    const tableBodyEl = document.querySelector('#scores');
+  
+    if (scores.length) {
+      for (const [i, score] of scores.entries()) {
+        const positionTdEl = document.createElement('td');
+        const nameTdEl = document.createElement('td');
+        const scoreTdEl = document.createElement('td');
+        const dateTdEl = document.createElement('td');
+  
+        positionTdEl.textContent = i + 1;
+        nameTdEl.textContent = score.name;
+        scoreTdEl.textContent = score.score;
+        dateTdEl.textContent = score.date;
+  
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(positionTdEl);
+        rowEl.appendChild(nameTdEl);
+        rowEl.appendChild(scoreTdEl);
+        rowEl.appendChild(dateTdEl);
+  
+        tableBodyEl.appendChild(rowEl);
+      }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+    }
+    //if there is data then the function creates a table with the scores, if there is no data it tells you to be the first one to set the score 
   }
-}
+  
+  loadScores();
+  //its loads the table with the datafunction loadScores() {
+  let scores = [];
+  const scoresText = localStorage.getItem('scores');
+  if (scoresText) {
+    scores = JSON.parse(scoresText);
+  }
+//first checks the storage to see if theres data
+  const tableBodyEl = document.querySelector('#scores');
 
-spinButton.addEventListener('click', rotateImage);
+  if (scores.length) {
+    for (const [i, score] of scores.entries()) {
+      const positionTdEl = document.createElement('td');
+      const nameTdEl = document.createElement('td');
+      const scoreTdEl = document.createElement('td');
+      const dateTdEl = document.createElement('td');
+
+      positionTdEl.textContent = i + 1;
+      nameTdEl.textContent = score.name;
+      scoreTdEl.textContent = score.score;
+      dateTdEl.textContent = score.date;
+
+      const rowEl = document.createElement('tr');
+      rowEl.appendChild(positionTdEl);
+      rowEl.appendChild(nameTdEl);
+      rowEl.appendChild(scoreTdEl);
+      rowEl.appendChild(dateTdEl);
+
+      tableBodyEl.appendChild(rowEl);
+    }
+  } else {
+    tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+  }
+  //if there is data then the function creates a table with the scores, if there is no data it tells you to be the first one to set the score 
+
+
+loadScores();
+//its loads the table with the data
