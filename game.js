f6ec0b76c87c2d669b131e4f25e3488a1e487915
Sean Stidham <seanstidham14@gmail.com>
2023-03-10 18:29:38 -0700

    game js edit

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
+const numSpins = document.getElementById("num-spins");
+const spinButton = document.getElementById("spin-button");
 
+let count = 0;
+
+spinButton.addEventListener("click", () => {
+  count++;
+  numSpins.value = count;
+});
