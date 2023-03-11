34963b378b2ad6984d449b33e73149ef7782380b
Sean Stidham <seanstidham14@gmail.com>
2023-03-10 19:09:53 -0700

    js+html edit edit

--- scores.js
+++ scores.js
@@ -5,6 +5,7 @@ const spinCounter = document.getElementById("num-spins");
 // Load scores from local storage or set empty array
 let scores = JSON.parse(localStorage.getItem("scores")) || [];
 
+// Render scores table
 function renderScores() {
   scoresTable.innerHTML = "";
   scores.forEach((score) => {
@@ -18,18 +19,18 @@ function renderScores() {
   });
 }
 
-
+// Save scores to local storage
 function saveScores() {
   localStorage.setItem("scores", JSON.stringify(scores));
 }
 
-
+// Update spin counter
 function updateSpinCounter() {
   const currentSpins = parseInt(spinCounter.value);
   spinCounter.value = currentSpins + 1;
 }
 
-
+// Handle login button click event
 function login() {
   const name = nameInput.value;
   if (name.trim() === "") {
@@ -37,24 +38,24 @@ function login() {
     return;
   }
 
-  
+  // Add new player to scores array
   scores.push({ name, spins: 0 });
 
-  
+  // Save scores and update table
   saveScores();
   renderScores();
 }
 
-
+// Handle spin button click event
 function spin() {
- 
+  // Update spin counter
   updateSpinCounter();
 
-  
+  // Check if player fails
   const isFail = Math.random() < 1 / 6;
   const currentPlayer = scores[scores.length - 1];
   if (isFail) {
-    
+    // Update fail date and save scores
     currentPlayer.failDate = new Date().toLocaleDateString();
     saveScores();
     renderScores();
@@ -64,8 +65,8 @@ function spin() {
   }
 }
 
-
+// Add event listeners to buttons
 document.getElementById("spin-button").addEventListener("click", spin);
 
-
+// Render scores table on page load
 renderScores();
