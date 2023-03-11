4778ea88a4dac8d859d1e2cd76bd072f2967fabd
Sean Stidham <seanstidham14@gmail.com>
2023-03-10 19:34:41 -0700

    js edit

--- scores.js
+++ scores.js
@@ -34,40 +34,4 @@ function loadScores() {
   }
   
   loadScores();
-  //its loads the table with the datafunction loadScores() {
-  let scores = [];
-  const scoresText = localStorage.getItem('scores');
-  if (scoresText) {
-    scores = JSON.parse(scoresText);
-  }
-//first checks the storage to see if theres data
-  const tableBodyEl = document.querySelector('#scores');
-
-  if (scores.length) {
-    for (const [i, score] of scores.entries()) {
-      const positionTdEl = document.createElement('td');
-      const nameTdEl = document.createElement('td');
-      const scoreTdEl = document.createElement('td');
-      const dateTdEl = document.createElement('td');
-
-      positionTdEl.textContent = i + 1;
-      nameTdEl.textContent = score.name;
-      scoreTdEl.textContent = score.score;
-      dateTdEl.textContent = score.date;
-
-      const rowEl = document.createElement('tr');
-      rowEl.appendChild(positionTdEl);
-      rowEl.appendChild(nameTdEl);
-      rowEl.appendChild(scoreTdEl);
-      rowEl.appendChild(dateTdEl);
-
-      tableBodyEl.appendChild(rowEl);
-    }
-  } else {
-    tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
-  }
-  //if there is data then the function creates a table with the scores, if there is no data it tells you to be the first one to set the score 
-
-
-loadScores();
-//its loads the table with the data
+ 
