e60c4e9b6864b00a95a82020caaa6ec0de33c226
Sean Stidham <seanstidham14@gmail.com>
2023-03-10 19:12:33 -0700

    js edit

--- scores.js
+++ scores.js
@@ -1,72 +1,73 @@
-const scoresTable = document.getElementById("scores");
-const nameInput = document.getElementById("name");
-const spinCounter = document.getElementById("num-spins");
-
-// Load scores from local storage or set empty array
-let scores = JSON.parse(localStorage.getItem("scores")) || [];
-
-// Render scores table
-function renderScores() {
-  scoresTable.innerHTML = "";
-  scores.forEach((score) => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${score.spins}</td>
-      <td>${score.name}</td>
-      <td>${score.failDate || "-"}</td>
-    `;
-    scoresTable.appendChild(row);
-  });
-}
-
-// Save scores to local storage
-function saveScores() {
-  localStorage.setItem("scores", JSON.stringify(scores));
-}
-
-// Update spin counter
-function updateSpinCounter() {
-  const currentSpins = parseInt(spinCounter.value);
-  spinCounter.value = currentSpins + 1;
-}
-
-// Handle login button click event
-function login() {
-  const name = nameInput.value;
-  if (name.trim() === "") {
-    alert("Please enter a valid name.");
-    return;
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+  //first checks the storage to see if theres data
+    const tableBodyEl = document.querySelector('#scores');
+  
+    if (scores.length) {
+      for (const [i, score] of scores.entries()) {
+        const positionTdEl = document.createElement('td');
+        const nameTdEl = document.createElement('td');
+        const scoreTdEl = document.createElement('td');
+        const dateTdEl = document.createElement('td');
+  
+        positionTdEl.textContent = i + 1;
+        nameTdEl.textContent = score.name;
+        scoreTdEl.textContent = score.score;
+        dateTdEl.textContent = score.date;
+  
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(positionTdEl);
+        rowEl.appendChild(nameTdEl);
+        rowEl.appendChild(scoreTdEl);
+        rowEl.appendChild(dateTdEl);
+  
+        tableBodyEl.appendChild(rowEl);
+      }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+    }
+    //if there is data then the function creates a table with the scores, if there is no data it tells you to be the first one to set the score 
   }
+  
+  loadScores();
+  //its loads the table with the datafunction loadScores() {
+  let scores = [];
+  const scoresText = localStorage.getItem('scores');
+  if (scoresText) {
+    scores = JSON.parse(scoresText);
+  }
+//first checks the storage to see if theres data
+  const tableBodyEl = document.querySelector('#scores');
 
-  // Add new player to scores array
-  scores.push({ name, spins: 0 });
+  if (scores.length) {
+    for (const [i, score] of scores.entries()) {
+      const positionTdEl = document.createElement('td');
+      const nameTdEl = document.createElement('td');
+      const scoreTdEl = document.createElement('td');
+      const dateTdEl = document.createElement('td');
 
-  // Save scores and update table
-  saveScores();
-  renderScores();
-}
+      positionTdEl.textContent = i + 1;
+      nameTdEl.textContent = score.name;
+      scoreTdEl.textContent = score.score;
+      dateTdEl.textContent = score.date;
 
-// Handle spin button click event
-function spin() {
-  // Update spin counter
-  updateSpinCounter();
+      const rowEl = document.createElement('tr');
+      rowEl.appendChild(positionTdEl);
+      rowEl.appendChild(nameTdEl);
+      rowEl.appendChild(scoreTdEl);
+      rowEl.appendChild(dateTdEl);
 
-  // Check if player fails
-  const isFail = Math.random() < 1 / 6;
-  const currentPlayer = scores[scores.length - 1];
-  if (isFail) {
-    // Update fail date and save scores
-    currentPlayer.failDate = new Date().toLocaleDateString();
-    saveScores();
-    renderScores();
-    alert("FAIL!");
+      tableBodyEl.appendChild(rowEl);
+    }
   } else {
-    alert("WINNER!");
+    tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
   }
-}
+  //if there is data then the function creates a table with the scores, if there is no data it tells you to be the first one to set the score 
 
-// Add event listeners to buttons
-document.getElementById("spin-button").addEventListener("click", spin);
 
-// Render scores table on page load
-renderScores();
+loadScores();
+//its loads the table with the data
