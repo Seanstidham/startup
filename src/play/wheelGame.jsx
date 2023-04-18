47393dfa1fcb91d92610e8c4aa6532a30ed894e0
Sean Stidham <seanstidham14@gmail.com>
2023-04-18 11:14:11 -0600

    game time: will it fix it, again, again

--- src/play/wheelGame.jsx
+++ src/play/wheelGame.jsx
@@ -1,53 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import './wheelGame.css';
 
-function App() {
+export function WheelGame(props) {
+  const userName = props.userName;
+  const [score, setScore] = React.useState(0);
+
+  function handleButtonPress() {
+    setScore(score + 1);
+  }
+
   return (
-    <div className="bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">Wheel of Fun Times</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="index.html">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="wheelgame.html">Play</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="scores.html">Scores</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="about.html">About</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <main>
-        <div className="players">
-          Player
-          <span className="player-name"></span>
-          <div id="player-messages"></div>
+    <div className='game'>
+      <div className='button-container'>
+        <img src='/image.png' alt='Give it a spin!' className='center' />
+        <div className='controls center'>
+          <div className='game-name'>Spin</div>
+          <div className='score center'>{score}</div>
+          <Button variant='primary' onClick={() => setScore(0)}>
+            Reset
+          </Button>
         </div>
-        <h1>Give it a spin!</h1>
-        <img src="https://static.vecteezy.com/system/resources/previews/001/192/278/non_2x/rainbow-spinning-wheel-png.png" />
-        <button id="spin-button" style={{marginTop: '0.25in', marginBottom: '0.25in'}}>Spin</button>
-        <div>
-          <label htmlFor="num-spins"># of spins:</label>
-          <input type="text" id="num-spins" value="0" readOnly />
-        </div>
-        <div className="text-center" id="result-message"></div>
-        <script src="game.js"></script>
-      </main>
+      </div>
     </div>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
